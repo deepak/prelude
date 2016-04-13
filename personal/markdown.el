406dc3e091dbe91963f371f63a0f95e3ccbff15f
Deepak Kannan <kannan.deepak@gmail.com>
2016-04-13 11:27:04 +0530

    Personalize markdown mode

---
+++ personal/markdown.el
@@ -0,0 +1,10 @@
+;;; Commentary
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (setq prelude-clean-whitespace-on-save nil)))
